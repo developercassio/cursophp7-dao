a963830a4c529e390ccc3b5db55ed2daf2b684e1
Cassio <desenvolvedor.cassio@gmail.com>
2018-09-03 17:34:02 -0400

    métodos de lista

--- index.php
+++ index.php
@@ -8,12 +8,30 @@
 
 	//echo json_encode($usuarios);
 
+/* //Carrega 1 usuario apenas pelo ID
 	$cassio = new Usuario();
 
 	$cassio->loadById(3);
 
 	
 	echo $cassio;
+*/
 
-	
+	//Carrega uma Lista de Usuários
+	//$lista = Usuario::getList();
+
+	//echo json_encode($lista);
+	//Carrega um lista de usuários buscando pelo login
+	//$search = Usuario::search("jo");
+
+	//echo json_encode($search);
+
+
+	//Carrega um usuário usando o login e senha
+	$usuario = new Usuario();
+
+	$usuario->login("teste", "dream123");
+
+	echo $usuario;
+
 ?>
