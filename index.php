42008cb4a5b9ab67d4e9904dc2d4a90431fbb333
Cassio <desenvolvedor.cassio@gmail.com>
2018-09-03 13:43:49 -0400

    Criamos a Classe Usuário

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 
 	require_once("config.php");
 
-	$sql = new Sql();
+	//$sql = new Sql();
 
-	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+	//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-	echo json_encode($usuarios);
+	//echo json_encode($usuarios);
+
+	$cassio = new Usuario();
+
+	$cassio->loadById(3);
+
+	
+	echo $cassio;
 
 	
 ?>
